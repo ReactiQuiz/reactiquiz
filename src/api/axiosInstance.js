04c268d16850fbbc82d2dbc3f29d3b739e805def
Sanskar Sontakke <sanskarsontakke@gmail.com>
2025-06-17 17:47:57 +0530

    new file:   backend/server.js 	modified:   backend/users.db 	modified:   src/api/axiosInstance.js

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -3,10 +3,10 @@
 import axios from 'axios';
 
 // The full URL of your deployed Vercel backend API
-const VERCEL_BACKEND_URL = 'https://reactiquiz.vercel.app/api'; 
+const VERCEL_BACKEND_URL = 'https://reactiquiz.vercel.app'; 
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || VERCEL_BACKEND_URL,
+  baseURL: VERCEL_BACKEND_URL,
 });
 
 export default apiClient;
