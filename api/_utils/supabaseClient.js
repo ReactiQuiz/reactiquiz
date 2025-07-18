5cbedbec2d2430239e29edd4ae6ee97983d4faa0
Sanskar Sontakke <sanskarsontakke@gmail.com>
2025-07-18 15:47:41 +0530

    modified:   api/_utils/supabaseClient.js

--- api/_utils/supabaseClient.js
+++ api/_utils/supabaseClient.js
@@ -1,8 +1,8 @@
 // api/_utils/supabaseClient.js
 const { createClient } = require('@supabase/supabase-js');
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
+const supabaseUrl = "https://ocdhqqwkxvvarrmxrzgg.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9jZGhxcXdreHZ2YXJybXhyemdnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwODAzMTksImV4cCI6MjA2NTY1NjMxOX0.FzizaXL860qFp0RLDnwwng8KBm0VJ_H7tTNvgOVCfPY";
 
 if (!supabaseUrl || !supabaseKey) {
     console.error("CRITICAL: Supabase URL or Service Key is missing. Check Vercel environment variables.");
