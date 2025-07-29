13c102b736a6e61b5c224d4303ccc49780867151
Sanskar Sontakke <sanskarsontakke@gmail.com>
2025-07-29 16:20:22 +0530

    modified:   src/components/AppRoutes.js

--- src/components/AppRoutes.js
+++ src/components/AppRoutes.js
@@ -2,8 +2,8 @@
 import React, { Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Box, CircularProgress } from '@mui/material';
-import { useAuth } from '../../contexts/AuthContext';
-import ProtectedRoute from './ProtectedRoute';
+import { useAuth } from '../contexts/AuthContext';
+import ProtectedRoute from './core/ProtectedRoute';
 
 const SuspenseFallback = () => (
     <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: 'calc(100vh - 128px)' }}>
@@ -11,22 +11,22 @@ const SuspenseFallback = () => (
     </Box>
 );
 
-const HomePage = React.lazy(() => import('../../pages/HomePage'));
-const AllSubjectsPage = React.lazy(() => import('../../pages/AllSubjectsPage'));
-const SubjectTopicsPage = React.lazy(() => import('../../pages/SubjectTopicsPage'));
-const DashboardPage = React.lazy(() => import('../../pages/DashboardPage'));
-const AccountPage = React.lazy(() => import('../../pages/AccountPage'));
-const LoginPage = React.lazy(() => import('../../pages/LoginPage'));
-const RegisterPage = React.lazy(() => import('../../pages/RegisterPage'));
-const ResultsPage = React.lazy(() => import('../../pages/ResultsPage'));
-const QuizPage = React.lazy(() => import('../../pages/QuizPage'));
-const AICenterPage = React.lazy(() => import('../../pages/AICenterPage'));
-const HomibhabhaPage = React.lazy(() => import('../../pages/HomibhabhaPage'));
-const AboutPage = React.lazy(() => import('../../pages/AboutPage'));
-const NotFoundPage = React.lazy(() => import('../../pages/NotFoundPage'));
-const QuizLoadingPage = React.lazy(() => import('../../pages/QuizLoadingPage'));
-const SettingsPage = React.lazy(() => import('../../pages/SettingsPage'));
-const FlashcardPage = React.lazy(() => import('../../pages/FlashcardPage')); // <-- ADD THIS IMPORT
+const HomePage = React.lazy(() => import('../pages/HomePage'));
+const AllSubjectsPage = React.lazy(() => import('../pages/AllSubjectsPage'));
+const SubjectTopicsPage = React.lazy(() => import('../pages/SubjectTopicsPage'));
+const DashboardPage = React.lazy(() => import('../pages/DashboardPage'));
+const AccountPage = React.lazy(() => import('../pages/AccountPage'));
+const LoginPage = React.lazy(() => import('../pages/LoginPage'));
+const RegisterPage = React.lazy(() => import('../pages/RegisterPage'));
+const ResultsPage = React.lazy(() => import('../pages/ResultsPage'));
+const QuizPage = React.lazy(() => import('../pages/QuizPage'));
+const AICenterPage = React.lazy(() => import('../pages/AICenterPage'));
+const HomibhabhaPage = React.lazy(() => import('../pages/HomibhabhaPage'));
+const AboutPage = React.lazy(() => import('../pages/AboutPage'));
+const NotFoundPage = React.lazy(() => import('../pages/NotFoundPage'));
+const QuizLoadingPage = React.lazy(() => import('../pages/QuizLoadingPage'));
+const SettingsPage = React.lazy(() => import('../pages/SettingsPage'));
+const FlashcardPage = React.lazy(() => import('../pages/FlashcardPage')); // <-- ADD THIS IMPORT
 
 function AppRoutes({ onOpenChangePasswordModal }) {
   const { currentUser, isLoadingAuth } = useAuth();
