d579d8739dfb99014f66aa3fd95ce6a20152e047
Sanskar Sontakke <sanskarsontakke@gmail.com>
2025-08-19 17:07:38 +0530

    modified:   src/components/layout/AdminLayout.js

--- src/components/layout/AdminLayout.js
+++ src/components/layout/AdminLayout.js
@@ -7,31 +7,24 @@ import AdminSidebar from '../admin/AdminSidebar';
 const drawerWidth = 240;
 
 function AdminLayout() {
-  // --- START OF CHANGES: Add state to manage sidebar open/closed status ---
   const [open, setOpen] = useState(true);
 
   const handleToggleDrawer = () => {
     setOpen(!open);
   };
-  // --- END OF CHANGES ---
 
   return (
-    // The main Box remains a flex container
     <Box sx={{ display: 'flex', minHeight: 'calc(100vh - 64px)' }}>
-      {/* --- START OF CHANGES: Pass state and handler to the sidebar --- */}
       <AdminSidebar 
         drawerWidth={drawerWidth} 
         open={open} 
         toggleDrawer={handleToggleDrawer} 
       />
-      {/* --- END OF CHANGES --- */}
       <Box
         component="main"
         sx={{
           flexGrow: 1,
           p: 3,
-          // The width calculation is no longer needed here.
-          // The flex-grow property will automatically fill the available space.
         }}
       >
         <Outlet />
